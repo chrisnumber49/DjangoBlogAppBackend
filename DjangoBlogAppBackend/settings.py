58ed3db8b4661736331f05a2f2122b9760e6817e
Chrisnumber49 <h28925847@gmail.com>
2021-11-16 05:21:36 +0800

    Django Blog App Backend

--- DjangoBlogAppBackend/settings.py
+++ DjangoBlogAppBackend/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
     }
 }
 # for Postgres on Heroku
-# DATABASES['default'] = dj_database_url.config(
-#     conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 
 # Password validation
